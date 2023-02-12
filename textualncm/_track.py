ab73dd02a925bf3f4c8844f88120bd85311254d5
Adrian Zhou <adrian.zhou@foxmail.com>
2023-02-12 10:37:28 -0800

    Added registry to Track class

--- textualncm/_track.py
+++ textualncm/_track.py
@@ -1,21 +1,33 @@
-from dataclasses import dataclass
+from weakref import WeakValueDictionary
 from rich.progress import Progress, BarColumn
 
 
-@dataclass
 class Track:
-    name: str
-    id: int
-    artists: str
-    album: str
-    album_id: int
-    local: bool | None = None
-    liked: bool | None = None
-    length: int = 0
-    size: int = 0
-    xfered: int = 0
-    _progress: Progress = Progress(BarColumn(), auto_refresh=False)
-    task = _progress.add_task('', total=None)
+    _registry = WeakValueDictionary()
+
+    def __init__(self, name, _id, artists, album, album_id):
+        self.name: str = name
+        self.id: int = _id
+        self.artists: str = artists
+        self.album: str = album
+        self.album_id: int = album_id
+        self.local: bool | None = None
+        self.liked: bool | None = None
+        self.length: int = 0
+        self.size: int = 0
+        self.xfered: int = 0
+        self._progress: Progress = Progress(BarColumn(), auto_refresh=False)
+        self.task = self._progress.add_task('', total=None)
+
+    def __new__(cls, name, _id, artists, album, album_id):
+        if _id in cls._registry:
+            return cls._registry[_id]
+        instance = super().__new__(cls)
+        cls._registry[_id] = instance
+        return instance
+
+    def __hash__(self):
+        return self.id
 
     @property
     def progress(self):
@@ -23,7 +35,6 @@ class Track:
             p.update(self.task, total=self.size, completed=self.xfered)
             return p
 
-    @dataclass
     class EmptyTrack:
         # The player receives this placeholder object at startup
         name: str = ''
@@ -31,6 +42,3 @@ class Track:
         local: bool = False
         liked: bool = False
         length: int = 0
-
-    def __hash__(self):
-        return self.id
